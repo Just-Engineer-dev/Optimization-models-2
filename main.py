a4d525cde09139a5a5794c0cc7ff9fcab9b686bc
Nikita Byvalin <nikitanikit9@gmail.com>
2023-09-17 23:48:58 +0300

    find max of lin function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import linprog
@@ -8,15 +9,15 @@ x2_1 = (8 - 4 * x1) / 2
 x2_2 = (6 - x1) / 3
 x2_3 = (4 - 4 * x1) / 3
 
-c = [4, 3]
+c = [-4, -3]  # Изменяем коэффициенты для максимизации
 
 A = [
-    [-4, -2],
-    [-1, -3],
-    [4, 3]
+    [4, 2],  # Изменяем знаки в ограничениях
+    [1, 3],  # Изменяем знаки в ограничениях
+    [-4, -3]  # Изменяем знаки в ограничениях
 ]
 
-b = [-8, -6, 4]
+b = [8, 6, -4]  # Изменяем знаки в ограничениях
 
 x0_bounds = (0, None)
 x1_bounds = (0, None)
@@ -31,7 +32,6 @@ plt.fill_between(x1, 0, x2_3, where=(x2_3 >= 0), color='gray', alpha=0.5)
 
 x2_level = (0 - 4 * x1) / 3
 
-
 plt.xlim(0, 5)
 plt.ylim(0, 5)
 plt.xlabel('x1')
@@ -43,8 +43,8 @@ plt.show()
 
 res = linprog(c, A_ub=A, b_ub=b, bounds=[x0_bounds, x1_bounds], method='simplex')
 
-min_x1 = res.x[0]
-min_x2 = res.x[1]
-min_z = res.fun
+max_x1 = res.x[0]
+max_x2 = res.x[1]
+max_z = -res.fun  # Меняем знак, так как ищем максимум
 
-print(f"Минимальное значение целевой функции: {min_z:.2f} при x1 = {min_x1:.2f}, x2 = {min_x2:.2f}")
+print(f"Максимальное значение целевой функции: {max_z:.2f} при x1 = {max_x1:.2f}, x2 = {max_x2:.2f}")
